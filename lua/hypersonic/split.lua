5a549bd425b721cde2e10869c05d2ba5f43e403e
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-02-02 21:59:17 +0100

    + capturing, trying for inserting elements

--- lua/hypersonic/split.lua
+++ lua/hypersonic/split.lua
@@ -4,6 +4,18 @@ local REGEX_TABLE = {}
 
 -- UILT
 
+-- print table
+local function print_table(tbl)
+	for _, elem in ipairs(tbl) do
+		if type(elem) == "table" then
+			print_table(elem)
+		else
+			print(elem)
+		end
+	end
+end
+
+
 -- split string letter by letter
 local function split_by_letter(str)
 	local letters = {}
@@ -21,7 +33,7 @@ local function split_by_letter(str)
 end
 
 -- Get length of array/table
--- len( []any ) => int 
+-- len( []any ) => int
 local function len(arr)
 	local i=0
 
@@ -30,6 +42,48 @@ local function len(arr)
 	end
 
 	return i
+end
+
+
+-- FUCK THIS SCHEME AND THIS FUNCTION
+-- New scheme for append function
+-- 	take TABLE, GROUP, VALUE as argument
+-- 	loop trough TABLE and each time you find new type=table
+-- 		add 1 to some index, if index == GROUP then 
+
+
+
+--	UPDATE, FUCK THIS SHIT TOO
+-- FIXME If i make 2 tables, one for chars and second for groups
+-- FIXME in chars i will save like { x,y,c, #groupX", {} }
+
+
+-- Append item to REGEX_TABLE
+-- append( []any, int, any(string))
+-- FIXME appending to another group
+-- FIXME OPTIONS:
+--		- group does not exists
+--		- it's saved in copy of variable
+local function append(tbl, group, value)
+	-- if is inserting to main arr
+	if group == 1 then
+		table.insert(REGEX_TABLE, value)
+		return
+	end
+
+	-- loop trough main arr
+	for _, val in ipairs(tbl) do
+		-- if main have table
+		if type(val) == "table" then
+			group = group - 1
+
+			if group == 1 then
+				table.insert(val, value)
+				return
+			end
+			append(val, group-1, value)
+		end
+	end
 end
 
 
@@ -80,32 +134,70 @@ local function is_class(type, letter)
 	end
 end
 
+-- TODO add other characters ($, ^, ...)
 
 -- CAPTURING
 
+local function capture_class(group, letter)
+	append(REGEX_TABLE, group, letter)
+end
 
 
 -- final function for spliting regex to groups
 local function split(regex)
 	local split_regex = split_by_letter(regex)
+	local regex_group = 1
+
+	-- false = can capture class, true = can capture everything
+	local can_capture = true
 
 	for i=1, len(split_regex) do
 		local letter = split_regex[i]
-		local prevLetter = split_regex[i-1]
+		local prev_letter = split_regex[i-1]
 
-		if is_literal_character(prevLetter) then
-			local char = prevLetter..letter
-			table.insert(REGEX_TABLE, char)
+		-- group check
+		-- FIXME
+		-- if is_group('start', letter) then
+		-- 	regex_group = regex_group + 1
+		-- elseif is_group('end', letter) then
+		-- 	regex_group = regex_group - 1
+		-- end
+
+		-- check for classes
+		if is_class('start', letter) then
+			can_capture = false
+		elseif is_class('end', letter) then
+			can_capture = true
+		end
+
+		-- CAPTURE
+		-- caprure in class
+		if can_capture == false then
+			capture_class(regex_group, letter)
+		end
+
+		-- capture literal characters
+		if is_literal_character(prev_letter) and can_capture then
+			local char = prev_letter..letter
+
+			append(REGEX_TABLE, regex_group, char)
+			i = i+1
+		end
+
+		-- capture character
+		if is_character(prev_letter, letter) and can_capture then
+			append(REGEX_TABLE, regex_group, letter)
 		end
 	end
 
 	-- just for removing warnings
-	print(split_regex())
-	print(is_literal_character())
-	print(is_character())
-	print(is_group())
-	print(is_class())
+	-- print(split_regex())
+	-- print(is_group())
+	-- print(is_class())
+	-- print(is_literal_character())
+	-- print(is_character())
+	print_table(REGEX_TABLE)
 end
 
 
-split('^([a-zA-Z])(*$)')
+split('^([a-zA-Z])(*$)ahoj')
